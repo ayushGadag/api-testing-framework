e243a9ab230d6b7d36abc2b58498028d55a9aecd
Ayush <ayushgadag09@gmail.com>
2026-03-24 21:27:01 +0530

    fix PUT and DELETE method

--- utils/api_client.py
+++ utils/api_client.py
@@ -10,12 +10,21 @@ HEADERS = {
 def get_users():
     return requests.get(f"{BASE_URL}/users?page=2", headers=HEADERS)
 
+
 def create_user(payload): 
     return requests.post(f"{BASE_URL}/users/add", json=payload, headers=HEADERS)
 
-def put_user_data(user_id,payload):
-    return requests.put(f"{BASE_URL}/users/{id}")
 
-def Delete_user_data(user_id):
-    return requests.delete(f"{BASE_URL}/user/{id}")
+def update_user(user_id, payload): 
+    return requests.put(
+        f"{BASE_URL}/users/{user_id}",   
+        json=payload,                   
+        headers=HEADERS                 
+    )
 
+
+def delete_user(user_id):   
+    return requests.delete(
+        f"{BASE_URL}/users/{user_id}",   
+        headers=HEADERS
+    )
