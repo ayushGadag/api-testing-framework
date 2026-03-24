320eed877faa8001a888784ee1d232526542bb76
Ayush <ayushgadag09@gmail.com>
2026-03-24 20:57:03 +0530

    Added POST API test and fixed API client

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-from utils.api_client import get_users
+from utils.api_client import get_users, create_user   # ✅ FIXED
 
 def test_get_users():
     response = get_users()
@@ -6,4 +6,19 @@ def test_get_users():
     assert response.status_code == 200
     
     json_data = response.json()
-    assert "users" in json_data   
+    assert "users" in json_data  
+
+
+def test_create_user():
+    payload = {
+        "name": "Ayush",
+        "job": "QA Engineer"
+    }
+
+    response = create_user(payload)
+
+    assert response.status_code == 200
+
+    json_data = response.json()
+
+    assert json_data["name"] == "Ayush"
