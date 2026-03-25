853f830677068fdcd6792b73c06c34a1438e6b8a
Ayush <ayushgadag09@gmail.com>
2026-03-25 20:49:01 +0530

    added the put and delete method

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
-from utils.api_client import get_users, create_user  
+from utils.api_client import get_users, create_user, update_user, delete_user
+
 
 def test_get_users():
     response = get_users()
@@ -22,3 +23,34 @@ def test_create_user():
     json_data = response.json()
 
     assert json_data["name"] == "Ayush"
+
+
+def test_update_user():
+    user_id = 1   # existing user
+
+    payload = {
+        "name": "Shravan",
+        "job": "Developer"
+    }
+
+    response = update_user(user_id, payload)
+
+    assert response.status_code == 200
+
+    json_data = response.json()
+
+    assert json_data["name"] == "Shravan"
+    assert "id" in json_data   # strong validation
+
+
+def test_delete_user():
+    user_id = 1
+
+    response = delete_user(user_id)
+
+    assert response.status_code == 201
+
+    json_data = response.json()
+
+    assert "id" in json_data   # confirm response contains id
+    
