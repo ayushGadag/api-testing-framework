9c66965d726884326642554a967b176e855a356c
Ayush <ayushgadag09@gmail.com>
2026-03-26 20:46:57 +0530

    Added schema validation and data-driven testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,16 @@
+import pytest
+from jsonschema import validate
 from utils.api_client import get_users, create_user, update_user, delete_user
+
+
+#  Schema for validation
+user_schema = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "number"}
+    },
+    "required": ["id"]
+}
 
 
 def test_get_users():
@@ -7,23 +19,34 @@ def test_get_users():
     assert response.status_code == 200
     
     json_data = response.json()
-    assert "users" in json_data  
+
+    assert "users" in json_data
+    assert len(json_data["users"]) > 0   # bonus validation
 
 
-def test_create_user():
+#  Data-driven testing (multiple inputs)
+@pytest.mark.parametrize("name, job", [
+    ("Ayush", "QA Engineer"),
+    ("Rahul", "Developer"),
+    ("shravani", "Tester")
+])
+def test_create_user(name, job):
+
     payload = {
-        "name": "Ayush",
-        "job": "QA Engineer"
+        "name": name,
+        "job": job
     }
 
     response = create_user(payload)
 
-    assert response.status_code == 201  
+    assert response.status_code == 201
 
     json_data = response.json()
 
-    assert "id" in json_data   
-    
+    assert "id" in json_data
+
+    # Schema validation
+    validate(instance=json_data, schema=user_schema)
 
 
 def test_update_user():
@@ -40,7 +63,10 @@ def test_update_user():
 
     json_data = response.json()
 
-    assert "id" in json_data   
+    assert "id" in json_data
+
+    # Schema validation
+    validate(instance=json_data, schema=user_schema)
 
 
 def test_delete_user():
@@ -48,8 +74,11 @@ def test_delete_user():
 
     response = delete_user(user_id)
 
-    assert response.status_code == 200   
+    assert response.status_code == 200
 
     json_data = response.json()
 
-    assert "id" in json_data   
+    assert "id" in json_data
+
+    # Schema validation
+    validate(instance=json_data, schema=user_schema)
